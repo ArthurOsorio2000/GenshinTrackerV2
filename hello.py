6af24aaa2afa488cbaa52ceb0173256f815de5e6
arthur osorio <ar1osorio@yahoo.com>
2024-08-27 15:48:40 +1000

    creating and testing rudimentary routing

--- hello.py
+++ hello.py
@@ -3,5 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def index():
+    return "Index Page"
+
+@app.route("/holly")
+def holly():
+    return "<p>Love u Holly!</p>"
